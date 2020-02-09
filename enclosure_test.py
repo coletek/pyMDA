ecbf4097b23eef7f3b0a7beed8f1bc3e149d951a
Luke Cole <git@coletek.org>
2020-02-09 14:35:05 +1000

    more work on enclosure helper

--- enclosure_test.py
+++ enclosure_test.py
@@ -6,38 +6,6 @@ from settings_common import *
 from enclosure_helpers import *
 import math
 
-CubePoints = [
-  [  0,  0,  0 ],  #0
-  [ 10,  0,  0 ],  #1
-  [ 10,  7,  0 ],  #2
-  [  0,  7,  0 ],  #3
-  [  0,  0,  5 ],  #4
-  [ 10,  0,  5 ],  #5
-  [ 10,  7,  5 ],  #6
-  [  0,  7,  5 ]] #7
-  
-CubeFaces = [
-  [0,1,2,3],  # bottom
-  [4,5,1,0],  # front
-  [7,6,5,4],  # top
-  [5,6,2,1],  # right
-  [6,7,3,2],  # back
-  [7,4,0,3]] # left
-  
-p = polyhedron(CubePoints, CubeFaces);
-
-#pts = []
-
-#p =
-
-c = cube([10, 10, 10])
-
-p = minkowski()(p, sphere(r=2, segments=segments_count)) - c
-
-p = minkowski()(p, sphere(r=2, segments=segments_count))
-
-# better way
-
 wall_thickness = 2.0
 draft_angle = math.radians(1.5)
 #draft_angle = math.radians(5.0)
@@ -57,7 +25,7 @@ cb = enclosure_corner_bottom(wall_thickness, curve_radius, draft_angle, height, 
 f = enclosure_face(height, length, wall_thickness, curve_radius, draft_angle)
 
 p = e + cb + ct + f
-p = e + f
+#p = e + f
 
 print(scad_render(p))
 exit()
