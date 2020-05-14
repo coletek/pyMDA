064106092bbe2ba5c8af0cffd822481dfcaa8eb9
Luke Cole <github@coletek.org>
2020-05-14 11:08:18 +1000

    fixed openscad_to_step.py helper

--- openscad_to_step.py
+++ openscad_to_step.py
@@ -1,13 +1,16 @@
+import os
 import sys
-
 sys.path.append('/usr/lib/freecad/lib')
-
 import FreeCAD
 import Part
 import importCSG
 
-def convert_openscad_to_step(scad_filename, step_filename, objname = 'difference')
+def convert_openscad_to_step(scad_filename, step_filename, objname = 'difference'):
+    
+    part_name = os.path.basename(scad_filename)
+    part_name = part_name.replace(".scad", "")
+    
     importCSG.open(scad_filename)
-    App.setActiveDocument(p)
-    part = FreeCAD.getDocument(p).getObject(objname)
+    FreeCAD.setActiveDocument(part_name)
+    part = FreeCAD.getDocument(part_name).getObject(objname)
     Part.export([part], step_filename)
