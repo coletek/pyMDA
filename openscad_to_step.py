da9a6ac8f98b2ef8eb54f55afad795cda94f9b2e
Luke Cole <github@coletek.org>
2020-05-14 10:47:25 +1000

    added objname

--- openscad_to_step.py
+++ openscad_to_step.py
@@ -6,9 +6,8 @@ import FreeCAD
 import Part
 import importCSG
 
-def convert_openscad_to_step(scad_filename, step_filename)
+def convert_openscad_to_step(scad_filename, step_filename, objname = 'difference')
     importCSG.open(scad_filename)
     App.setActiveDocument(p)
-    objname = "difference"
     part = FreeCAD.getDocument(p).getObject(objname)
     Part.export([part], step_filename)
